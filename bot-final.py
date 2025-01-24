9768fc8fc0c7f75fb1b9a5bc00a50be2b09a5320
r4pt3r <adarshk460@gmail.com>
2025-01-24 19:03:36 +0000

    Added bot-final.py for direct push without any api, updated requirements.txt

--- bot-final.py
+++ bot-final.py
@@ -1,10 +1,11 @@
-import requests, json, re, os, mysql.connector, uvicorn
+import requests, json, re, os, mysql.connector
 from dotenv import load_dotenv
 from fastapi import FastAPI, Query
 from mysql.connector import Error
 from datetime import datetime
+from bs4 import BeautifulSoup
 
-app = FastAPI() #API service
+
 load_dotenv() #Load credentials
 
 API_TOKEN = os.getenv('API_TOKEN')
@@ -36,14 +37,25 @@ def compare_ranks(asin, cat_rank, subcat_rank, cat_rank_int, subcat_rank_int): #
     else:
         return f'ASIN: {asin} 🔽\n{cat_rank} \n{subcat_rank}'
 
-def api_rank(asin): # get ranks from the API
-    api = f'http://localhost:8000/rank?asin={asin}'
-    res = requests.get(api)
-    data = json.loads(res.text)
+def get_rank(asin): # get ranks from the API
+    url = f'https://www.amazon.in/dp/{asin}'
+    headers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246"}
 
-    asin_p = data["asin"]
-    cat_rank = data["category_rank"] # with category names
-    subcat_rank = data["sub_category_rank"] # with category names
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    # get the division of product detail rankings
+    table = soup.find('div', {'id': 'productDetails_db_sections', 'class': 'a-section'})
+    if not table:
+        raise ValueError("No valid ASIN found!")
+    
+    # filtered span with rank data
+    span_comp = table.find_all("span")
+
+    # seperate the rankings
+    cat_rank = span_comp[1].text.replace("(See Top 100 in Home & Kitchen)", "").strip()
+    #cat_rank = re.sub(r"\(.*?\)", "", span_comp[1]).strip()
+    subcat_rank = span_comp[2].text
 
     cat_rank_int = re.search(r'\d{1,3}(,\d{3})*',cat_rank).group() # extracting numbers alone
     subcat_rank_int = re.search(r'\d{1,3}(,\d{3})*',subcat_rank).group() # extracting numbers alone
@@ -60,7 +72,7 @@ def send_msg(asin): # send message to telegram group
 
     payload = {
         'chat_id': CHAT_ID,
-        'text': api_rank(asin)
+        'text': get_rank(asin)
     }
 
     response = requests.post(url, data=payload)
@@ -127,19 +139,9 @@ def insert_table(asin, cat_rank, subcat_rank): # Insert into db
 
     db_close(connection, cursor) 
 
-# listenting for api
-@app.get("/asin")
-async def get_asin(
-    asin: str = Query(..., description="Amazon Standard Identification Number (ASIN)")
-    ):
-    # Extract ASIN from URL
-    asin_match = re.search(r'dp/([A-Z0-9]+)', asin)
-    if asin_match:
-        asin = asin_match.group(1)
-    create_table_asin(asin) # creates tables
-    send_msg(asin) # sends message
-
 # main function where asin is passed
 if __name__ == "__main__":
     print(db_conn())
-    uvicorn.run(app, host="0.0.0.0", port=9000)
+    asin = 'B09TSVL68H'
+    create_table_asin(asin) # creates tables
+    send_msg(asin) # sends message
