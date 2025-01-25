6d16661347225556a6219cb69fe981ed725ebb22
r4pt3r <adarshk460@gmail.com>
2025-01-25 15:41:25 +0000

    Fixed comparison error and Added Track DB for recursively run for all asins in the table

--- bot-final.py
+++ bot-final.py
@@ -24,12 +24,10 @@ def compare_ranks(asin, cat_rank, subcat_rank, cat_rank_int, subcat_rank_int): #
     SELECT time, cat_rank, subcat_rank from {asin} 
     ORDER BY time DESC LIMIT 1;
     """
-
     cursor.execute(get_last_record_query)
 
     results = cursor.fetchone() 
-    print(results[1])
-
+        
     if results[1]==str(cat_rank_int):
         return f'ASIN: {asin} ✅\n{cat_rank} \n{subcat_rank}'
     elif results[1]<str(cat_rank_int):
@@ -44,27 +42,25 @@ def get_rank(asin): # get ranks from the API
     response = requests.get(url, headers=headers)
     soup = BeautifulSoup(response.content, 'html.parser')
 
-    # get the division of product detail rankings
-    table = soup.find('div', {'id': 'productDetails_db_sections', 'class': 'a-section'})
+    table = soup.find('div', {'id': 'productDetails_db_sections', 'class': 'a-section'})  # get the division of product detail rankings
     if not table:
         raise ValueError("No valid ASIN found!")
     
-    # filtered span with rank data
-    span_comp = table.find_all("span")
+    span_comp = table.find_all("span") # filtered span with rank data
 
-    # seperate the rankings
-    cat_rank = span_comp[1].text.replace("(See Top 100 in Home & Kitchen)", "").strip()
+    cat_rank = span_comp[1].text.replace("(See Top 100 in Home & Kitchen)", "").strip() # seperate the rankings
     #cat_rank = re.sub(r"\(.*?\)", "", span_comp[1]).strip()
     subcat_rank = span_comp[2].text
 
     cat_rank_int = re.search(r'\d{1,3}(,\d{3})*',cat_rank).group() # extracting numbers alone
     subcat_rank_int = re.search(r'\d{1,3}(,\d{3})*',subcat_rank).group() # extracting numbers alone
-
-    insert_table(asin, cat_rank_int , subcat_rank_int) #insert to db after removing un-wanted texts
     
     #message = f'ASIN: {asin_p} \n{cat_rank} \n{subcat_rank}'
+    print(f'compare_ranks({asin}, {cat_rank}, {subcat_rank}, {cat_rank_int}, {subcat_rank_int})')
     message = compare_ranks(asin, cat_rank, subcat_rank, cat_rank_int, subcat_rank_int)
     #send_msg(message)
+
+    insert_table(asin, cat_rank_int , subcat_rank_int) #insert to db after removing un-wanted texts
     return message
 
 def send_msg(asin): # send message to telegram group
@@ -113,7 +109,7 @@ def create_table_asin(asin): # Create a table
     """
     cursor.execute(create_table_query)
     print("Table created successfully!")
-    db_close(connection, cursor)
+    send_msg(asin)
 
 def insert_table(asin, cat_rank, subcat_rank): # Insert into db
     connection, cursor = db_conn()
@@ -137,11 +133,26 @@ def insert_table(asin, cat_rank, subcat_rank): # Insert into db
     connection.commit()
     print("Data inserted successfully!")        
 
-    db_close(connection, cursor) 
+def track_asin(table_name):
+    connection, cursor = db_conn()
 
+    select_query = f"""
+    SELECT asin, sku, chat_id FROM {table_name}
+    """
+    cursor.execute(select_query)
+    results = cursor.fetchall()
+
+    print(results)
+
+    for row in results:
+        asin = row[0]
+        sku = row[1]
+        chat_id = row[2]
+
+        create_table_asin(asin)
+        
 # main function where asin is passed
 if __name__ == "__main__":
-    print(db_conn())
-    asin = 'B09TSVL68H'
-    create_table_asin(asin) # creates tables
-    send_msg(asin) # sends message
+    connection, cursor = db_conn()
+    track_asin('track')
+    db_close(connection, cursor)
